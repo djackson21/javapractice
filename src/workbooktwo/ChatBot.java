2e66c71ddbce38a7969316acd271927e1e2851aa
Derron Jackson <Derrnjackson01gmail.com>
2022-11-01 15:56:03 -0500

    Created a chat bot

---
+++ src/workbooktwo/ChatBot.java
@@ -0,0 +1,43 @@
+package workbooktwo;
+
+import java.util.Scanner;
+
+public class ChatBot {
+    public static void main(String[] args) {
+        //See Learn the Part for the complete instructions.
+
+        //set up scanner./
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Hello. What is your name?");
+        String name = scan.nextLine();
+
+
+        //add new a line before asking next question.
+        System.out.println("\nHi " + (name) + "! I'm Javabot. Where are you from?");
+        //Pick up user's home and store it.
+        String home = scan.nextLine();
+
+
+        //add new a line before asking next question.
+        System.out.println("I hear it's beautiful at " + (home) + "! I'm from a place called Oracle");
+        System.out.println("\nHow old are you?");
+        //Pick up age and store it.
+        int age = scan.nextInt();
+
+
+        //add new a line before asking next question.
+        System.out.println("So you're " + (age) + ", cool! I'm 400 years old.");
+        System.out.println("\nThis means I'm "+ (400/age) + " times older than you.");
+        System.out.println("\nEnough about me. What's your favourite language? (just don't say Python)");
+        //Pick up language and store it.
+        scan.nextLine();
+        String language = scan.nextLine();
+
+
+        //add new a line here.
+        System.out.println(""+(language)+", that's great! Nice chatting with you "+(name)+". I have to log off now. See ya "+ name+ "!");
+        scan.close();
+        //close scanner.
+    }
+}
